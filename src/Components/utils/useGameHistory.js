bdc5dd983fa41d24b661adbb800aa3dfb80805cc
JayvonK <jaykhuth@gmail.com>
2024-03-05 23:17:26 -0800

    more pseudo

---
+++ src/Components/utils/useGameHistory.js
@@ -0,0 +1,8 @@
+//Pseudo
+//Create a function that returns an object holding 5 elements, an array, a number, a fuction that updates history, another function that resets history, and a function that is used to change the state of the number
+//We initialize the state of the array as an array with the length of 9 and that's filled with null
+//We initialize the state of the number as 0
+//We need the update history function to take in a parameter, then set the array we created to that parameter, and also set the number we created to the parameter's length minus 1
+//We need the reset history function to reset the array and integer to it's original values
+
+
