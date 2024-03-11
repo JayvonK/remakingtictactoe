e3994ab69fb9d2506ee7370e221ff6094f5b1028
JayvonK <jaykhuth@gmail.com>
2024-03-11 06:37:39 -0700

    more pseudo

--- src/Components/Game/GameComponent.js
+++ src/Components/Game/GameComponent.js
@@ -1,5 +1,7 @@
 //Pseudo
-//Creating a component that returns a parent div, with a child div, that has and title of tic tac toe
+//Creating a component that returns a parent div, with a child div, that has and title of tic tac toe, theres another child div that has a boardcomponent as it's grandchild, with the properties of squares and an onclick
+//At the top we use destructuring to assign history, stepNumber, updateHistory, resetHistory, and setStepNumber to access all the returns from our useGameHistory() function
+//We set currentSquares equal to 
 
 
 
