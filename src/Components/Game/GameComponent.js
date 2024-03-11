c0343a8feb70ae1334d882d9e8502b093f88e966
JayvonK <jaykhuth@gmail.com>
2024-03-10 20:59:55 -0700

    pseudo

---
+++ src/Components/Game/GameComponent.js
@@ -0,0 +1,16 @@
+//Pseudo
+//Creating a component that returns a parent div, with a child div, that has and title of tic tac toe
+
+
+
+import React from 'react'
+
+const GameComponent = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GameComponent
